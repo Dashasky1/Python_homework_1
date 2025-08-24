7b6fbdb47fd9bdb559ae9eafb929ed011e7e78c5
Dashasky1 <dariaermolenko29@gmail.com>
2025-08-24 12:54:05 +1000

    lesson_3

--- SKY/04_lesson/test_string_utils.py
+++ SKY/04_lesson/test_string_utils.py
@@ -46,26 +46,28 @@ def test_capitalize_negative(input_str, expected):
     ("hello world", " "),
 ])
 def test_contains_positive(string, symbol):
-    Utiles2 = StringUtils()
-    assert Utiles2.contains(string, symbol) is True
+    assert string_utils.contains(string, symbol) is True
 
 
 @pytest.mark.negative
 @pytest.mark.parametrize("string, symbol", [
     ("skypro", "z"),])
 def test_contains_negative(string, symbol):
-    Utiles2 = StringUtils()
-    assert not Utiles2.contains(string, symbol) is False
+    assert string_utils.contains(string, symbol) is False
 
 
 @pytest.mark.positive
+@pytest.mark.parametrize("string, symbol, expected_result", [
+    ("skypro", "s", "kypro"),
+    ("SkyPro", "P", "SkyRo")
+])
 def test_delete_symbol_positive(string, symbol, expected_result):
     assert string_utils.delete_symbol(string, symbol) == expected_result
-    assert string_utils.delete_symbol("skypro", "s") == "kypro"
-
-
 
 @pytest.mark.negative
+@pytest.mark.parametrize("string, symbol, expected_result", [
+    ("skypro", "1", "skypro"),
+    ("", "a", "")
+])
 def test_delete_symbol_negative(string, symbol, expected_result):
     assert string_utils.delete_symbol(string, symbol) == expected_result
-    assert string_utils.delete_symbol("skypro", "1") == "skypro"
