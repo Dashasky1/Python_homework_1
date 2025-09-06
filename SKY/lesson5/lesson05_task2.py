fa2d3c8dbf511c9d464aa7f080e9b89d323e4ae4
Dashasky1 <dariaermolenko29@gmail.com>
2025-09-07 00:04:41 +1000

    lesson_3

---
+++ SKY/lesson5/lesson05_task2.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.maximize_window()
+
+driver.get("http://uitestingplayground.com/dynamicid")
+
+elements = driver.find_elements(By.CLASS_NAME, 'btn btn-primary')
+
+print("кнопка найдена и нажата")
